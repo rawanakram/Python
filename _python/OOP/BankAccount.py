ad2b6b07b45961a15b3171a01904859fd9918b7e
rawanakram <rawan20181997@hotmail.com>
2023-05-10 22:50:19 -0700

    bank account initial commit

---
+++ _python/OOP/BankAccount.py
@@ -0,0 +1,31 @@
+class BankAccount:
+	def __init__(self, int_rate, balance):
+        self.int_rate = int_rate
+        self.balance = 0
+	
+	def deposit(self, amount):
+		self.balance+=amount
+        return self
+
+	def withdraw(self, amount):
+		self.balance-=amount
+        return self
+
+	def display_account_info(self):
+		print("Balance:",self.balance,"$")
+        return self
+
+	def yield_interest(self):
+        if balance > 0:
+            self.balance = balance + (balance*int_rate)
+        return self
+
+
+Omar = BankAccount(0.01, 2000)
+Emy = BankAccount(0.01, 1200)
+
+#To the first account, make 3 deposits and 1 withdrawal, then yield interest and display the account's info  
+Omar.deposit(1000).deposit(800).deposit(120).withdraw(1200).display_account_info()
+
+#To the second account, make 2 deposits and 4 withdrawals, then yield interest and display the account's info
+Emy.deposit(800).deposit(900).withdraw(120).withdraw(1000).withdraw(200).withdraw(150).display_account_info()
