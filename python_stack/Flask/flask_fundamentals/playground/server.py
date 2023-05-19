ff1c807531a852d095cbdcda4603cd9d40985526
rawanakram <rawan20181997@hotmail.com>
2023-05-18 17:11:08 -0700

    playground

---
+++ python_stack/Flask/flask_fundamentals/playground/server.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template  
+app = Flask(__name__)                     
+
+@app.route("/play")    
+@app.route("/play/<int:x>")
+@app.route("/play/<int:x>/<string:color>")                           
+def playground(x=3, color="skyblue"):
+    return render_template('index.html', num=x, color=color )
+    
+if __name__=="__main__":
+    app.run(debug=True)                   
+
